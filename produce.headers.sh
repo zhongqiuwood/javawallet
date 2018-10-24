a70697c677aeefa0c3eb844671a6f26db1b6b7b8
zhongqiuwood <zhongqiuwood@gmail.com>
2018-10-24 18:10:30 +0800

    rename to xrb

--- produce.headers.sh
+++ produce.headers.sh
@@ -7,5 +7,6 @@ javah -jni com.okcoin.vault.jni.zcash.ZcashNativeInvoke
 javah -jni com.okcoin.vault.jni.dash.DashNativeInvoke
 javah -jni com.okcoin.vault.jni.qtum.QtumNativeInvoke
 javah -jni com.okcoin.vault.jni.digibyte.DigibyteNativeInvoke
+javah -jni com.okcoin.vault.jni.xrb.Raiblocksj
 
 mv com_okcoin_vault_jni_*.h $ORIGINAL_DIR
