b480e06ffc5116046967693d9e6c3dd11be107f2
xiangjianmeng <805442788@qq.com>
2018-10-30 03:48:14 -0400

    Update Hcj.java

--- src/main/java/com/okcoin/vault/jni/hc/Hcj.java
+++ src/main/java/com/okcoin/vault/jni/hc/Hcj.java
@@ -12,42 +12,44 @@ public class Hcj {
 
     static public native String[] execute(String command);
 
-    public static void executeCommand(String cmd, String context) {
+    public static String[] executeCommand(String cmd, String context) {
 
         String[] results = Hcj.execute(cmd);
         Util.dump(context, results);
+        return results;
     }
 
     public static void main(String[] args)
     {
         try {
             /* open hcwallet */
-            cmd = "openhcwallet -u rpcuser -P rpcpass --pass 123456 --testnet";
+            String[] results;
+            cmd = "openhcwallet -u rpcuser -P rpcpass --pass 123456";
             Hcj.executeCommand(cmd, "openhcwallet");
 
-            String seckey = "PtWU8TjP3KBZJQ2ycoakD6sLLotyW2Ftss6CaroazJVJhzayTemwV";
+            String seckey = "9995335dfb8074beb2ff6b207e0fd32d6883273fa3d730955ebca7100f904954";
             cmd ="getaddressbyprivatekey " + seckey;
             Hcj.executeCommand(cmd, "getaddressbyprivatekey");
 
             /* create unsigned transaction */
             String numoftxin = "1 ";
-            String txin1 = "953f8c4fc1688ff4a2e8622003797535fb9c97ffee97b36fb82fe0ddba327723 1 ";
-            String toamount = "Tsp4P1rZZf3TevgkG8M8BH8kxpyzfCN5oR9 50000000 ";
-            String chargeamount = "Tsai18bMCXwiyB4cC3gGE1pHqZ7zNDHjCJQ 140000000";
+            String txin1 = "3f34b7feb5af823258dfd806370dc604b0953b8f9e0abe89a637d6ce8d40907d 0 ";
+            String toamount = "HsDU3Mz8EvffEBuCEqJwiLoVAAUDhwXsMXr 30000000 ";
+            String chargeamount = "HsMDowUazGqhVwEXyh8AdUcm66hGGW6gW2f 160000000";
             cmd = "createrawtransaction " + numoftxin + txin1 + toamount + chargeamount;
-            Hcj.executeCommand(cmd, "createrawtransaction");
+            results = Hcj.executeCommand(cmd, "createrawtransaction");
 
             
             /* sign raw transation */
-            String rawTx = "0100000001237732badde02fb86fb397eeff979cfb357579032062e8a2f48f68c14f8c3f950100000000ffffffff02003b58080000000000001976a9146a43a3cd20d7353dd9f6459520d697976911c3a288ac80f0fa020000000000001976a914fcb7dcaa8a2b9a214045d5de40efc470bbba6d4a88ac000000000000000001ffffffffffffffff00000000ffffffff00 ";
-            String privkeys = "PtWU8TjP3KBZJQ2ycoakD6sLLotyW2Ftss6CaroazJVJhzayTemwV";
+            String rawTx = results[0] + " ";
+            String privkeys = "9995335dfb8074beb2ff6b207e0fd32d6883273fa3d730955ebca7100f904954";
             cmd = "signrawtransaction " + rawTx + privkeys;
-            Hcj.executeCommand(cmd, "signrawtransaction");
+            results = Hcj.executeCommand(cmd, "signrawtransaction");
 
             /* send raw transation */
-            String signedTx = "0100000001237732badde02fb86fb397eeff979cfb357579032062e8a2f48f68c14f8c3f950100000000ffffffff02003b58080000000000001976a9146a43a3cd20d7353dd9f6459520d697976911c3a288ac80f0fa020000000000001976a914fcb7dcaa8a2b9a214045d5de40efc470bbba6d4a88ac000000000000000001ffffffffffffffff00000000ffffffff6a47304402206d010e0810c98f33775bc4cf2dce296c585b70d21e86decb6bcc4a4a4de751140220299f555f0a42e3fa73a938b07b7740f0523286cc992b3e515822f6b57538384d0121023e4356d4d44eb16dd16c8146e4c530c6adaaae0b5c7a3cb38917630f9e2e58d4";
+            String signedTx = results[0];
             cmd = "sendsignedtransaction " + signedTx;
-            Hcj.executeCommand(cmd, "sendsignedtransaction");
+            results =  Hcj.executeCommand(cmd, "sendsignedtransaction");
 
             /* close hcwallet */
             cmd = "closehcwallet";
